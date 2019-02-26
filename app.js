0992e319da279bbff4ff1f67925957dc61cfd0c0
biodun1994 <olapadeabiodun21@gmail.com>
2019-02-26 19:11:57 +0100

    AJAX POST fully implemented with fields RESET

--- app.js
+++ app.js
@@ -1,14 +1,38 @@
 $(document).ready(() =>{
 
     $("#main-form").on("submit", (e) =>{
+        
+        //prevent submit default behaviour
         e.preventDefault();
+
+        //creating student obj from accepted data
         const student = {
             fName: $("#fName").val(),
             sName:$("sName").val(),
             cadre: $("#cadre").val()
-        }
-        console.log(student);
-    })
+        };
+
+        //making AJAx POST call
+        $.ajax({
+            type: "POST",
+            url: "http://localhost:3000/students",
+            data: student,
+            dataType: "json",
+            encode: true
+        })
+        .done(() =>{
+            console.log("Data posted successfully")
+        })
+        .fail(() =>{
+            console.log("Error postion data")
+        });
+
+        //reset form
+        $("input[type='text']").val("");
+        $('select option[value="Junior"]').attr("selected",true);
+
+        // console.log(student);
+    });
 
 
 
